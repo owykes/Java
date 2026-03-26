22c0b31e7452a8a674d9b45bd6ef2383df1a8665
owykes <owenwykes1992@gmail.com>
2026-03-26 19:24:39 +0000

    setup java project folder, first couple of exercises from the head first java book

---
+++ TenGreenBottles.java
@@ -0,0 +1,22 @@
+public class TenGreenBottles {
+    public static void main(String[] arg) {
+    int numBottles = 10;
+    String word = "bottles";
+    
+    while (numBottles > 0) {
+
+        if (numBottles == 1) {
+            word = "bottle";
+        }
+
+        System.out.println(numBottles + " green " + word + ", hanging on the wall");
+        System.out.println(numBottles + " green " + word + ", hanging on the wall");
+        System.out.println("And if one green bottle should accidently fall");
+        numBottles = numBottles - 1;
+
+        if (numBottles > 0) {
+            System.out.println("There'll be " + )
+        }
+    }
+    }
+}
