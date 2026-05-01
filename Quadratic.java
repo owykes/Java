b25b23f37a18da8756cde134cf8048f5fd16ffc0
owykes <owenwykes1992@gmail.com>
2026-05-01 18:13:06 +0100

    finished the quadratic exercise

--- Quadratic.java
+++ Quadratic.java
@@ -3,7 +3,34 @@ import java.util.Scanner;
 public class Quadratic {
 
     public static void equation(int a, int b, int c) {
+        // refuse division by zero
+        if (a == 0) {
+            System.err.println("You cannot divide by zero in a quadratic equation");
+            return;
+        }
         
+        //sum under the square root
+        double discriminant = b * b - 4 * a * c;
+        // refuse a square root of a negative number
+        if (discriminant < 0) {
+            System.err.println("You cannot take a square root of a negative number");
+            return;
+
+        } else if (discriminant == 0) {
+                double root = -b / (2.0 * a);
+                System.out.println("One solution: " + root);
+                return;
+            
+        } else {
+            double numeratorPositive = -b + Math.sqrt(discriminant);
+            double numeratorNegative = -b - Math.sqrt(discriminant);
+            double denominator = 2.0 * a;
+        
+            double root1 = numeratorPositive / denominator;
+            double root2 = numeratorNegative / denominator;
+
+            System.out.printf("root one = %.2f and root two = %.2f", root1, root2);
+        }        
     }
 
     public static void main(String[] args) {
