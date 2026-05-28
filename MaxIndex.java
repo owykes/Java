0907b16e79c713c254a68cfa4be47e8b6ae13e50
owykes <owenwykes1992@gmail.com>
2026-05-28 22:25:08 +0100

    enhanced loop maxindex

---
+++ MaxIndex.java
@@ -0,0 +1,16 @@
+public class MaxIndex {
+
+    public static void indexOfMax(int [] a) {
+        int largest = 0;
+
+        for ( int value : a) {
+            if ;
+        }
+        // enhanced for loop ? returns index of largest integar 
+
+
+
+
+            return largest;
+    }
+}
