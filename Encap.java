fc9ac002f8397c22a6fea82dd23ee0324b163f67
owykes <owenwykes1992@gmail.com>
2026-07-08 13:16:52 +0100

    encap exerscise to finish immutable chapter

---
+++ Encap.java
@@ -0,0 +1,15 @@
+public class Encap {
+    public static int parenthesesCheck(String s) {
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if ( c == '(') {
+                count++;    
+            } else if (c == ')') {
+                count--;
+            }
+        }
+        return count;
+    }
+}
--- Expansion.java
+++ Expansion.java
@@ -4,18 +4,30 @@ public class Expansion {
         int result = 1;
         for (int i = 2; i <= n; i++) {
             result *= i;
+        }
         return result;
     }
 
     public static double power(double x, int n) {
-        
-    }
-    public static int myexp(double x, int n) {
-        //
-        int sum = 0;
+        double result = 1;
         for (int i = 0; i < n; i++) {
-            sum += power(x, i) / factorial(i);
+            result *= x;
         }
+        return result;
+    }
 
+    public static double myexp(double x, int n) {
+        // Taylor series estimation
+        double sum = 1;
+        double numerator = 1;
+        double denominator = 1;
+        
+        for (int i = 1; i < n; i++) {
+            numerator *= x;
+            denominator *= i;
+            double term = numerator / denominator;
+            sum += term;
+        }
+        return sum;    
     }
 }
